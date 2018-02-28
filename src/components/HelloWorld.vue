41dfb90f68f4cb158947ff2fc0460dd4745d5fab
revodoge <revodoge@users.noreply.github.com>
2018-02-27 22:31:49 -0600

    update default board to the ultimate board with 20 solutions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,7 +38,7 @@
     name: 'HelloWorld',
     data() {
       return {
-        sides: [3, 3, 1, 2, 2, 2, 1, 4, 4, 2, 3, 3, 1, 3, 2, 2, 2, 3, 1, 2],
+        sides: [2, 2, 2, 1, 3, 2, 2, 3, 3, 1, 2, 2, 2, 1, 3, 2, 2, 3, 3, 1],
         boardSize: 5,
         towersList: {},
         row: [],
